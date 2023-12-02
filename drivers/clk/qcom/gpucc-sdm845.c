b1bdf01e5af6c4b61719fe095df6542ea2b98470
mikooomich <mikooomichael@gmail.com>
2023-12-01 23:01:13 -0500

    Use VDD_GX_HIGH for 710 Mhz GPU clocks instead of VDD_GX_HIGH_L1
    Change-Id: I3486711342f65a0b44f76c8357c758a449139ae9

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -638,7 +638,7 @@ static void gpu_cc_gfx_sdm845_fixup_sdm845v2(void)
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_NOMINAL_L1] =
 		596000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 675000000;
-	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L1] =
+	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] =
 		710000000;
 }
 
